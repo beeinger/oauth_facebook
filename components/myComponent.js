16883d5696c28f7dfef63363fec1116ef15f10e7
beeinger <sulecki.maciek@gmail.com>
2019-10-23 22:57:28 +0200

    tests

--- components/myComponent.js
+++ components/myComponent.js
@@ -2,20 +2,41 @@ import React from "react";
 import FacebookLogin from "react-facebook-login";
 
 class MyComponent extends React.Component {
-  responseFacebook(response) {
-    console.log(response);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      response: false
+    };
+  }
+  responseFacebook(resp) {
+    console.log(resp);
+    var that = this;
+    fetch("https://stark-retreat-68154.herokuapp.com/facebook", {
+      method: "post",
+      body: JSON.stringify(resp)
+    }).then(function(res) {
+      that.setState({ response: res.json() });
+    });
   }
 
   render() {
     return (
-      // https://www.npmjs.com/package/react-facebook-login - for info about editting
-      <FacebookLogin
-        appId="2173246342969843"
-        autoLoad={true}
-        fields="name,email,picture"
-        scope="public_profile,user_friends"
-        callback={this.responseFacebook}
-      />
+      <div>
+        {/* https://www.npmjs.com/package/react-facebook-login - for info about editting */}
+        <FacebookLogin
+          appId="2173246342969843"
+          autoLoad={true}
+          fields="name,email,picture"
+          scope="public_profile,user_friends"
+          callback={() => this.responseFacebook}
+        />
+        {this.state.response ? (
+          <h1>{JSON.stringify(this.state.response)}</h1>
+        ) : (
+          <h1>Wait patiently</h1>
+        )}
+      </div>
     );
   }
 }
