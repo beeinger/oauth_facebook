8ae97eb7bcb3481dcf4a37bd655cf05792588967
beeinger <sulecki.maciek@gmail.com>
2019-10-23 21:04:02 +0200

    trying stuff out

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,25 @@ import MyComponent from "../components/myComponent.js";
 
 const Home = () => (
   <div>
+    <div id="fb-root"></div>
+    <script
+      async
+      defer
+      crossorigin="anonymous"
+      src="https://connect.facebook.net/pl_PL/sdk.js#xfbml=1&version=v4.0&appId=2173246342969843&autoLogAppEvents=1"
+    ></script>
     <Head>
       <title>Home</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <MyComponent />
+    <div
+      class="fb-login-button"
+      data-width=""
+      data-size="large"
+      data-button-type="continue_with"
+      data-auto-logout-link="true"
+      data-use-continue-as="true"
+    ></div>
   </div>
 );
 
